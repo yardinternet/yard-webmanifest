3b82a7de796521c913c44ad6de3009c80bd11422
Freek de Kruijff <freek.dekruijff@yard.nl>
2024-11-21 17:09:46 +0100

    (chore): add manual config option

--- config/webmanifest.php
+++ config/webmanifest.php
@@ -11,4 +11,20 @@ return [
     // https://developer.mozilla.org/en-US/docs/Web/Manifest/theme_color
     'background_color' => '',
     'theme_color' => '',
+
+    /**
+     * Set icons manually
+     * allowed attributes are: src, sizes, type, purpose
+     *
+     * Example:
+     *
+     * 'icons' => [
+     *   [
+     *      'src' => 'path/to/icon.png',
+     *      'sizes' => '192x192',
+     *      'type' => 'image/png',
+     *   ],
+     * ],
+     */
+    'icons' => [],
 ];
--- src/Data/WebmanifestIconData.php
+++ src/Data/WebmanifestIconData.php
@@ -11,4 +11,5 @@ class WebmanifestIconData extends Data
     public string $src;
     public string $sizes;
     public string $type;
+    public string $purpose;
 }
