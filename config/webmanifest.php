65a47b8f06fd714414679c91f10a7965818e5086
Freek de Kruijff <freek.dekruijff@yard.nl>
2024-11-22 09:34:08 +0100

    (refactor): change colour

--- config/webmanifest.php
+++ config/webmanifest.php
@@ -6,15 +6,13 @@ return [
     'url' => '/manifest.webmanifest',
     'iconSizes' => [192, 384, 512, 1024],
 
-    // Default Theme settings
-    // https://developer.mozilla.org/en-US/docs/Web/Manifest/background_color
-    // https://developer.mozilla.org/en-US/docs/Web/Manifest/theme_color
+    // Theme color settings
     'background_color' => '',
     'theme_color' => '',
 
     /**
-     * Set icons manually
-     * allowed attributes are: src, sizes, type, purpose
+     * Set icons manually,
+     * allowed attributes are: src, sizes, type, purpose.
      *
      * Example:
      *
