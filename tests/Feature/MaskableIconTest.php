7d3670fef80a70d0f9c4d849799f635a2950e68e
Freek de Kruijff <freek.dekruijff@yard.nl>
2025-01-02 13:32:23 +0100

    (chore): update tests

--- tests/Feature/MaskableIconTest.php
+++ tests/Feature/MaskableIconTest.php
@@ -2,17 +2,32 @@
 
 declare(strict_types=1);
 
+use Yard\Webmanifest\Data\IconData;
 use Yard\Webmanifest\MaskableIcon;
+
+it('can create filename', function () {
+	$iconData = IconData::from([
+		'name' => 'testIcon',
+		'size' => 512,
+		'extension' => 'png',
+	]);
+
+	expect($iconData->getFileName())->tobe('testIcon_512.png');
+});
 
 it('can get base64 icon from transiet', function () {
 	$maskableIcon = new MaskableIcon(\Intervention\Image\ImageManager::gd());
 
-	$size = 192;
+	$iconData = IconData::from([
+		'name' => 'testIcon',
+		'size' => 512,
+		'extension' => 'png',
+	]);
 
 	WP_Mock::userFunction('get_transient')
 		->once()
-		->with("Base64 icon {$size}px")
+		->with($iconData->getFileName())
 		->andReturn('ICON BASE64');
 
-	expect($maskableIcon->getIcon($size))->tobe('ICON BASE64');
+	expect($maskableIcon->getBase64Icon($iconData))->tobe('ICON BASE64');
 });
